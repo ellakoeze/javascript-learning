469e21a80ad4e3e5385b560620e9ed82ad676382
ella-koeze <ekoeze@jomc7890.dhcp.unc.edu>
2015-01-29 09:44:46 -0500

    logic day

---
+++ logic.js
@@ -0,0 +1,34 @@
+// sample js file
+
+x = 10; //this is a variable
+userName = "Ella";//string
+teams = new Array();
+
+teams = ["Panthers", "Seahawks", "Colts" ];
+
+wins = [8, 12, 11];
+losses = [4,1,2];
+
+console.log(teams);
+
+console.log("wins: " + wins);
+console.log("losses: " + losses);
+
+console.log(teams.length);
+
+document.write("The " + teams[0] +" had " + wins[0] + " wins. </br>");
+
+document.write(" There are " + teams.length +" teams in the league. </br>");
+
+for (var i=0; i<teams.length; i++){
+    //console.log(teams[i]);
+    //document.write("The " + teams[i] +" had " + wins[0] + " wins. </br>");
+    if (wins [i] >= 10) {
+    console.log(teams[i]+" are a good team");
+
+} else {
+    console.log (teams[i]+" are not great");
+}
+}
+
+
